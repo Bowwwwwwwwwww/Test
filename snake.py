5ef907522816a68062b8411bdee8f1411e38a4f2
Bowwwwwwwwwww <lutinsaro@gmail.com>
2023-10-01 11:19:03 +0200

    first commit

---
+++ snake.py
@@ -0,0 +1,100 @@
+import pyxel as px
+from collections import deque
+import random
+
+# CONSTANTE pour la couleur de fond
+BG_COLOR = px.COLOR_BLACK
+# CONSTANTE pour la couleur du serpent
+SNAKE_COLOR = px.COLOR_WHITE
+# CONSTANTE pour la couleur de la nourriture
+FOOD_COLOR = px.COLOR_RED
+
+def update():
+    global snake, dx, dy, food, loose
+
+    if loose:
+        if px.btnp(px.KEY_SPACE):
+            init_game()
+            loose = False
+        return 
+
+    # Gestion des événements utilisateur
+    if px.btnp(px.KEY_UP) and dy != 1:
+        dx = 0
+        dy = -1
+    elif px.btnp(px.KEY_DOWN) and dy != -1:
+        dx = 0
+        dy = 1
+    elif px.btnp(px.KEY_RIGHT) and dx != -1:
+        dx = 1
+        dy = 0
+    elif px.btnp(px.KEY_LEFT) and dx != 1:
+        dx = -1
+        dy = 0
+
+    # novelle tete du serpent 
+    new_tete = (snake[0][0] + dx, snake[0][1] + dy)
+
+    # Si la tete du serpent est aux extremiter de l'ecran sa fait perdre la partie 
+    if (new_tete[0] < 0 or new_tete[0] >= px.width or new_tete[1] < 0 or new_tete[1] >= px.height):
+        loose = True
+        return  #le jeu est perdu 
+
+    # Pour savoir si la tete est dans le corps du serpent pour perdre la partie 
+    if new_tete in snake:
+        loose = True
+        return
+
+    # Vérifier si le serpent mange de la nourriture
+    if new_tete == food:
+        # on remet la "pomme"/nourriture a un endroit aleatoire de l'ecran 
+        food= (random.randint(0, px.width - 1), random.randint(0, px.height - 1))
+    else:
+        # on retire pour permettre d'avancer 
+        snake.pop()
+
+    # on ajoute la nouvelle tete a la deque 
+    snake.appendleft(new_tete)
+
+def draw():
+    global snake, food, loose
+
+    px.cls(BG_COLOR)  # Effacer l'écran
+
+    # Dessiner la nourriture
+    px.pset(food[0], food[1], FOOD_COLOR)
+
+    if loose:
+        # c'est pour afficher perdue quand le joueur a loose 
+        px.text(1, 1, "PERDUE", px.COLOR_RED)
+        px.text(1, 20, "ESPACE POUR JOUER", px.COLOR_GREEN)
+    else:
+        # Dessiner le serpent
+        for segment in snake:
+            px.pset(segment[0], segment[1], SNAKE_COLOR)
+
+def init_game():
+    global snake, dx, dy, food, loose
+
+    # Coordonnées de départ du serpent (une deque de segments)
+    snake = deque([(px.width // 2, px.height // 2)])
+
+    # Direction initiale du serpent
+    dx = 1
+    dy = 0
+
+    # Coordonnées de départ de la nourriture à une position aléatoire
+    food= (random.randint(0, px.width - 1), random.randint(0, px.height - 1))
+
+    loose = False  # Réinitialiser le statut du jeu
+
+def init():
+    global snake, dx, dy, food, loose
+
+    px.init(25, 25, fps=10)  # Initialiser un écran de 50x50 pixels à 10 FPS
+
+    init_game()
+
+    px.run(update, draw)  # Lancer la boucle des fonctions update et draw
+
+init()
